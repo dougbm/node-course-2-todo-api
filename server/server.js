aff1f4198843ee5e4a9bf8172fef8fb56442437a
Douglas <douglasmarquardt@gmail.com>
2017-07-24 17:02:44 -0300

    Synking home repos

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -23,14 +23,14 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
-})
+  });
+});
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
-    res.status(400).send();
+    res.status(400).send(e);
   });
 });
 
@@ -92,13 +92,24 @@ app.patch('/todos/:id', (req, res) => {
 
     res.send({todo});
   }).catch((e) => {
-    res.status(404).send();
+    res.status(400).send();
   })
+});
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
